cd9c59f617ba11d2538c11d3be70fb5d462e85e4
prajith2252 <83197685+prajith2252@users.noreply.github.com>
2022-07-18 12:58:59 +0530

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -72,13 +72,13 @@ class TgUploader:
         if keption.startswith('/'): keption = keption.replace('/', '', 1)
         # print full path file location -
         if CUSTOM_FILENAME is not None:
-            cap_mono = f"{CUSTOM_FILENAME} <code>{keption}</code>" #f"{CUSTOM_FILENAME} <code>{file_}</code>"
+            cap_mono = f"{CUSTOM_FILENAME} <b>{keption}</b>" #f"{CUSTOM_FILENAME} <code>{file_}</code>"
             file_ = f"{CUSTOM_FILENAME} {file_}"
             new_path = ospath.join(dirpath, file_)
             osrename(up_path, new_path)
             up_path = new_path
         else:
-            cap_mono = f"<code>{keption}</code>" #cap_mono = f"<code>{file_}</code>"
+            cap_mono = f"<b>{keption}</b>" #cap_mono = f"<code>{file_}</code>"
         notMedia = False
         thumb = self.__thumb
         self.__is_corrupted = False
